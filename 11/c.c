e863ff9c74a9a3c909e23f72ffd75c6cd134b9f9
SEUNGHYEON20 <163938068+SEUNGHYEON20@users.noreply.github.com>
2024-05-21 11:43:14 +0900

    Add files via upload

---
+++ 11/c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define AL 5
+
+int main()//선택정렬????????????????????????
+{
+    int s_list[AL]={5,4,3,2,1};
+    int a,b,temp,sort;
+
+    printf("초기> 배열 요소: 5,4,3,2,1\n");
+    printf("\n[for 문으로 선택정렬 수행]\n");
+
+    for(a=0; a<AL-1; a++)
+    {
+        sort=a;
+
+        for(b=a+1; b<AL; b++)
+        {
+            if(s_list[b] < s_list[sort])
+            {
+                sort=b;
+            }
+        }
+        temp=s_list[a];
+        s_list[a]=s_list[sort];
+        s_list[sort]=temp;
+    }
+    printf("결과> 배열 요소: ");
+
+    for(a=0; a<AL; a++)
+    {
+        printf("%d", s_list[a]);
+    }
+    return 0;
+}
