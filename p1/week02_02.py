15083bb6b27346125204fcc402d8a10dd0428b67
SEUNGHYEON20 <163938068+SEUNGHYEON20@users.noreply.github.com>
2024-09-04 16:03:00 +0900

    Add files via upload

---
+++ p1/week02_02.py
@@ -0,0 +1,34 @@
+# week02_02.py
+
+
+
+
+print("내년에는 몇 살일까?")
+# 올해 나이 입력 받는다.
+age=int(input("올해 나이:"))
+# 내년 나이를 출력한다.
+print("내년 나이:", age+1)
+
+
+
+print("#4")
+score1=input("점수 1:")
+score2=input("점수 2:")
+print(score1 + score2)
+
+print("#5")
+score1=int(input("점수 1:"))
+score2=int(input("점수 2:"))
+print(score1 + score2)
+
+
+
+print("#1")
+input()
+
+print("#2")
+input("점수:")
+
+print("#3")
+a = input("점수:")
+input(a)
